b5991b2b6a38fc6f25a3da0d54357cbfab105921
Debabrata <debabrata@parseable.com>
2025-07-14 07:59:57 +0530

    Release v0.2.0: Fix Material Table rendering issues, update UI terminology, and improve code structure

--- parseable-backstage-plugin/packages/app/src/components/Root/Root.tsx
+++ parseable-backstage-plugin/packages/app/src/components/Root/Root.tsx
@@ -75,7 +75,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-        <SidebarItem icon={AssessmentIcon} to="parseable-logstream" text="Parseable Logstream" />
+        <SidebarItem icon={AssessmentIcon} to="parseable-logstream" text="Parseable Dataset" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         {/* End global nav */}
         <SidebarDivider />
